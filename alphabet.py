278bfcaa91f6f84c70020bd700f071ede632e5d6
ryuji0123 <ryo1011.javelin@gmail.com>
2020-10-23 13:03:41 +0900

    add alphabet

---
+++ alphabet.py
@@ -0,0 +1,6 @@
+def num2alpha(num):
+    if num <= 26:
+        return chr(ord('a') - 1 + num)
+    if num % 26 == 0:
+        return num2alpha(num // 26 - 1)+chr(ord('z'))
+    return num2alpha(num // 26)+chr(ord('a') - 1 + num % 26)
