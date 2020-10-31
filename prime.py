876c4760258bf04165e17f135659260503623ee8
ryuji0123 <ryo1011.javelin@gmail.com>
2020-10-31 14:03:24 +0900

    modify prime

--- prime.py
+++ prime.py
@@ -1,20 +1,7 @@
-from math import sqrt
-
-def factorization(n):
-    arr = []
-    tmp = n
-    for div in range(2, int(sqrt(n)) + 2):
-        if tmp % div == 0:
-            cnt = 0
-            while tmp % div == 0:
-                cnt += 1
-                tmp //= div
-            arr.append([div, cnt])
-
-    if tmp != 1:
-        arr.append([tmp, 1])
-
-    if len(arr) == 0:
-        arr.append([n, 1])
-
-    return arr
+def isPrime(x):
+    i = 2
+    while i * i <= x:
+        if x % i == 0:
+            return False
+        i += 1
+    return True
