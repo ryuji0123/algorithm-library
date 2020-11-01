de208fdfd84cbde56dd1d5900cbc79aa411837d8
ryuji0123 <ryo1011.javelin@gmail.com>
2020-11-01 15:09:17 +0900

    update prime

--- prime.py
+++ prime.py
@@ -5,3 +5,20 @@ def isPrime(x):
             return False
         i += 1
     return True
+
+
+def prime_factorize_set(n):
+    ret = set()
+    while n % 2 == 0:
+        ret.add(2)
+        n //= 2
+    i = 3
+    while i * i <= n:
+        if n % i == 0:
+            ret.add(i)
+            n //= i
+        else:
+            i += 2
+    if n != 1:
+        ret.add(n)
+    return ret
