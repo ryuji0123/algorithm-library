19c6bb675780bb083677b7071850daf3c318210a
ryuji0123 <ryo1011.javelin@gmail.com>
2020-11-03 15:12:39 +0900

    modify union_find.py

--- union_find.py
+++ union_find.py
@@ -8,3 +8,16 @@ class UnionFind:
     def root(self, x):
         self.par[x] = self.root(self.par[x]) if self.par[x] != x else x
         return self.par[x]
+
+class UnionFind:
+    def __init__(self):
+        self.par = {}
+
+    def unite(self, x, y):
+        self.par[x] = self.par.setdefault(x, x)
+        self.par[y] = self.par.setdefault(y, y)
+        self.par[self.root(x)] = self.root(y)
+
+    def root(self, x):
+        self.par[x] = self.root(self.par[x]) if self.par[x] != x else x
+        return self.par[x]
