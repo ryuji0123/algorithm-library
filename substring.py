10a3d1b1d24cb06de604dc726c0ce006b5fbb853
ryuji0123 <ryo1011.javelin@gmail.com>
2020-11-07 21:07:04 +0900

    modify substring.py

--- substring.py
+++ substring.py
@@ -8,3 +8,12 @@ def generateAllSubstrings(word):
 def getAllSubstrings(word):
     length = len(word)
     return [word[i: j] for i in range(length) for j in range(i + 1, length + 1)]
+
+
+def generateOneCharacterChangedWord(word):
+    length = len(word)
+    for idx in range(length):
+        for o in range(ord('a'), ord('z') + 1):
+            c = chr(o)
+            if c != word[idx]:
+                yield word[: idx] + c + word[idx + 1: ]
