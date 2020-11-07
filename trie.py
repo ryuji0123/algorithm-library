cec32740f3fd1c4ff802d467da512563498bc4b4
ryuji0123 <ryo1011.javelin@gmail.com>
2020-11-07 21:58:04 +0900

    modify trie.py

--- trie.py
+++ trie.py
@@ -18,6 +18,27 @@ class Trie:
                 return False
             t = t[c]
         return True if self.eow in t else False
+        # return t.get(self.eow, 0)
+
+    def simSearch(self, word):
+        stack = deque([(self.tree, 0, 0)])
+        ret = 0
+        while stack:
+            t, idx, is_changed = stack.pop()
+            if idx == len(word):
+                ret += is_changed * t.get(self.eow, 0)
+                continue
+
+            if word[idx] in t:
+                stack.append([t[word[idx]], idx + 1, is_changed])
+
+            if is_changed == 1:
+                continue
+
+            for k in t.keys():
+                if k != self.eow and k != word[idx]:
+                    stack.append([t[k], idx + 1, 1])
+        return ret
 
     def contain(self, word):
         t = self.tree
