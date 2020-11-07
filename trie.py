b3c72e56f0efccead2546eec1bf716035f417bb8
ryuji0123 <ryo1011.javelin@gmail.com>
2020-11-07 20:41:06 +0900

    add trie.py

---
+++ trie.py
@@ -0,0 +1,28 @@
+class Trie:
+    def __init__(self):
+        self.tree = {}
+        self.eow = 'EOW'
+
+    def add(self, word):
+        t = self.tree
+        for c in word:
+            if c not in t:
+                t[c] = {}
+            t = t[c]
+        t[self.eow] = None
+
+    def search(self, word):
+        t = self.tree
+        for c in word:
+            if c not in t:
+                return False
+            t = t[c]
+        return True if self.eow in t else False
+
+    def contain(self, word):
+        t = self.tree
+        for c in word:
+            if c not in t:
+                return False
+            t = t[c]
+        return True
