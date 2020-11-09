c0642e52678ee9e97bc1cdbd5e3d481e5eff032a
ryuji0123 <ryo1011.javelin@gmail.com>
2020-11-09 10:57:41 +0900

    modify div.py

--- div.py
+++ div.py
@@ -1,7 +1,20 @@
 from math import gcd
 
+
 def lcm(x, y):
     return (x * y) // gcd(x, y)
+
+
+from math import gcd
+
+
+def gcdFromLeftAndRight(N, A):
+    L = [0] * (N + 1)
+    R = [0] * (N + 1)
+    for idx in range(1, N + 1):
+        L[idx] = gcd(L[idx - 1], A[idx - 1])
+        R[N - idx] = gcd(R[N + 1 - idx], A[N - idx])
+    return L, R
 
 
 def make_divisors(n):
