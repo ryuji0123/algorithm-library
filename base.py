66448c9029bc2acbb02525157fad3c31d9ae82d5
ryuji0123 <ryo1011.javelin@gmail.com>
2020-11-16 09:18:39 +0900

    add base.py

---
+++ base.py
@@ -0,0 +1,14 @@
+def convertNegativeBase(N, neg_base):
+    if (N == 0):
+        return '0'
+
+    ret = []
+    while (N != 0):
+        div = int(N / neg_base)
+        remainder = N - neg_base * div
+        if remainder < 0:
+            div += 1
+            remainder -= neg_base
+        N = div
+        ret.append(str(remainder))
+    return ''.join(reversed(ret))
