a0903c40ce06a87a5fbcf22c24bd819fe21f7e7b
ryuji0123 <ryo1011.javelin@gmail.com>
2020-11-20 23:08:24 +0900

    merge files

--- div.py
+++ div.py
@@ -27,3 +27,26 @@ def make_divisors(n):
                 upper_divisors.append(n//i)
         i += 1
     return lower_divisors + upper_divisors[::-1]
+
+
+from math import sqrt
+
+
+def factorization(n):
+    arr = []
+    tmp = n
+    for div in range(2, int(sqrt(n)) + 2):
+        if tmp % div == 0:
+            cnt = 0
+            while tmp % div == 0:
+                cnt += 1
+                tmp //= div
+            arr.append([div, cnt])
+
+    if tmp != 1:
+        arr.append([tmp, 1])
+
+    if len(arr) == 0:
+        arr.append([n, 1])
+
+    return arr
