9999a6a099af0c3b5a0b663c06e7c469e076f638
ryuji0123 <ryo1011.javelin@gmail.com>
2020-11-28 22:44:43 +0900

    modify ./lcm.py

--- lcm.py
+++ lcm.py
@@ -1,9 +1,13 @@
 from math import gcd
+
+
 def lcm_base(x, y):
     return (x * y) // gcd(x, y)
 
+
 def lcm_list(numbers):
     return reduce(lcm_base, numbers, 1)
+
 
 def lcm(*numbers):
     return reduce(lcm_base, numbers, 1)
